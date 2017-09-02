693a1c292410e8abcccb831dbe8d58171d5c55b3
whhub <wanghui122004@me.com>
2017-09-02 15:45:27 +0800

    [002] Add Project HelloOpenGL

---
+++ HelloOpenGL/program.cpp
@@ -0,0 +1,16 @@
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLEW
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	return 0;
+}
