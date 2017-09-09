c80b8fc19398c34cf357c98f800b6058c1e00f06
whhub <wanghui122004@me.com>
2017-09-09 10:42:30 +0800

    [008] [Extract Method] Draw Triangle

--- HelloOpenGL/program.cpp
+++ HelloOpenGL/program.cpp
@@ -51,13 +51,6 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 
-GLfloat vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-};
-
-
 void CheckShaderCompileStatus(GLuint vertexShader)
 {
     GLint Success;
@@ -68,6 +61,54 @@ void CheckShaderCompileStatus(GLuint vertexShader)
         glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         cout << "ERROR::SHADER:COMPILATION_FAILED\n" << infoLog << endl;
     }
+}
+
+void DrawTriangle(GLFWwindow* window, GLuint shaderProgram)
+{
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	//创建顶点缓冲对象， ID 是 1
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// 向顶点缓冲对象写入数据
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+
+	// Game Loop
+	while(!glfwWindowShouldClose(window))	// glfw 监听是否需要需要退出
+	{
+		glfwPollEvents();	// 检查是否有事件处理函数
+
+		// 渲染指令
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);   // unbind VBO
+
+		glfwSwapBuffers(window);	// 交换双缓冲
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 }
 
 int main()
@@ -136,44 +177,8 @@ int main()
 	// 清屏参数
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-	//创建顶点缓冲对象， ID 是 1
-	GLuint VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);
-	
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	DrawTriangle(window, shaderProgram);
 
-	// 向顶点缓冲对象写入数据
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
-
-	// Game Loop
-	while(!glfwWindowShouldClose(window))	// glfw 监听是否需要需要退出
-	{
-		glfwPollEvents();	// 检查是否有事件处理函数
-
-		// 渲染指令
-		glClear(GL_COLOR_BUFFER_BIT);
-
-        // Draw our first triangle
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);   // unbind VBO
-
-		glfwSwapBuffers(window);	// 交换双缓冲
-	}
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
 	
 	glfwTerminate();	// 释放 GLFW 分配的内存	
 
