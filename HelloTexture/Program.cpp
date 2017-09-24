b4ee4e9d0edabb5560d89684a9322673208bac91
whhub <wanghui122004@me.com>
2017-09-24 23:21:30 +0800

    [030][HelloTexture] vertexShader fragmentShader

--- HelloTexture/Program.cpp
+++ HelloTexture/Program.cpp
@@ -1,5 +1,6 @@
 #include "AfxStd.h"
 #include "Program.h"
+#include "soil.h"
 
 void DrawCabinetTexture(GLFWwindow* window, GLuint shaderProgram)
 {
@@ -11,6 +12,22 @@ void DrawCabinetTexture(GLFWwindow* window, GLuint shaderProgram)
     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
+
+    // 生成纹理
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+
+    // 加载并生成纹理
+    int width, height;
+    unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
     GLuint VAO, VBO;
 
     glGenVertexArrays(1, &VAO);
