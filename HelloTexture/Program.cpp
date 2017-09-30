e8457dedb7c489f96d0fd7e112ed2af89b37ad61
whhub <wanghui122004@me.com>
2017-09-30 20:14:15 +0800

    [035][Refactor] move GenTexture() from project HelloTexture to TextureFactory

--- HelloTexture/Program.cpp
+++ HelloTexture/Program.cpp
@@ -1,26 +1,7 @@
 #include "AfxStd.h"
 #include "Program.h"
-#include "soil.h"
+#include "TextureFactory.h"
 
-GLuint GenTexture(const char * textureFilePath)
-{
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-
-    // 为当前绑定的纹理对象设置环绕、过滤方式
-
-    // 加载并生成纹理
-    int width, height;
-    unsigned char* image = SOIL_load_image(textureFilePath, &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    SOIL_free_image_data(image);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    return texture;
-}
 
 void DrawCabinetTexture(GLFWwindow* window, GLuint shaderProgram)
 {
---
+++ include/TextureFactory.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <GL/glew.h>
+
+GLuint GenTexture(const char * textureFilePath);
