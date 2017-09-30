e9b6cb9240c9e9f44596d8cfd39570cd5484225b
whhub <wanghui122004@me.com>
2017-09-30 21:46:10 +0800

    [038][HelloTexture] [exercise 01] 仅让笑脸图案朝向另一个方向

--- HelloTexture/twoTexturedFragment.fs
+++ HelloTexture/twoTexturedFragment.fs
@@ -9,5 +9,5 @@ uniform sampler2D ourTexture2;
 
 void main()
 {
-	color = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), 0.2);
+	color = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, vec2(1.0f - TexCoord.x, TexCoord.y) ), 0.2);
 }
